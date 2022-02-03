6dcbd3d926c0c6d2802ecac659329b50f32b1257
Nick Chang <nnchang01@gmail.com>
2022-02-03 15:47:19 -0800

    kind of a lot of changes
    but now it handles searches for users and for venues!

--- iotm/scrape.js
+++ iotm/scrape.js
@@ -3,28 +3,43 @@ const axios = require("axios");
 const fs = require("fs");
 const Papa = require('papaparse');
 
-/**
- * load the URL
- * re-run with '.meta.pagination.links.next', if it exists
-*/
-function loadFromIotmUrl(url) {
+const venueColumns = ['title', 'capacity', 'website', 'facebook', 'yelp', 'twitter', 'phone', 'address', 'city', 'state', 'created_at', 'song_types', 'categories', 'description'];
+const bandColumns = ['display_name', 'website', 'songkick', 'soundcloud', 'city', 'state', 'created_at', 'genres', 'bio'];
+
+function processBand(bandOrVenue) {
+    const trimmedData = _.pick(bandOrVenue, bandColumns);
+    trimmedData.genres = trimmedData.genres?.data.map(genreobj => genreobj.genre);
+    trimmedData.songkick = trimmedData.songkick ?? 'https://www.songkick.com/artists/' + trimmedData.songkick;
+    trimmedData.soundcloud = trimmedData.soundcloud ?? 'https://soundcloud.com/' + trimmedData.soundcloud;
+    const hasWebsite = trimmedData.website && trimmedData.website.toLowerCase().indexOf("myspace.com") === -1;
+    if (!hasWebsite && !trimmedData.soundcloud && !trimmedData.songkick) {
+        return null;
+    }
+    return trimmedData;
+}
+
+function processVenue(bandOrVenue) {
+    const trimmedData = _.pick(bandOrVenue, venueColumns);
+    trimmedData.categories = trimmedData.categories?.data.map(categoryObj => categoryObj.name).join(', ');
+    const hasWebsite = trimmedData.website && trimmedData.website.toLowerCase().indexOf("myspace.com") === -1;
+    if (!hasWebsite && !trimmedData.facebook && !trimmedData.phone) {
+        return null;
+    }
+    return trimmedData;
+}
+
+function loadFromIotmUrl(url, isVenue) {
+    if (!isVenue && url.indexOf('users') === -1) {
+        return Promise.reject('URL was not for band or venue');
+    }
     return axios.get(url)
         .then(response => {
-            // see data type information at the bottom
-            const retrievedData = [];
-            response.data.data.forEach(bandOrVenue => {
-                const trimmedData = _.pick(bandOrVenue, ['display_name', 'website', 'songkick', 'soundcloud', 'city', 'state', 'created_at', 'genres', 'bio']);
-                trimmedData.genres = trimmedData.genres.data.map(genreObj => genreObj.genre);
-                trimmedData.songkick = trimmedData.songkick ?? 'https://www.songkick.com/artists/' + trimmedData.songkick;
-                trimmedData.soundcloud = trimmedData.soundcloud ?? 'https://soundcloud.com/' + trimmedData.soundcloud;
-                const hasWebsite = trimmedData.website && trimmedData.website.toLowerCase().indexOf("myspace.com") === -1;
-                if (hasWebsite || trimmedData.soundcloud || trimmedData.songkick) {
-                    retrievedData.push(trimmedData);
-                }
-            })
+            const retrievedData = response.data.data
+                .map(bandOrVenue => isVenue ? processVenue(bandOrVenue) : processBand(bandOrVenue))
+                .filter(processed => processed != null);
             const maybeNextLink = response.data.meta.pagination.links.next;
             if (maybeNextLink) {
-                return loadFromIotmUrl(maybeNextLink)
+                return loadFromIotmUrl(maybeNextLink, isVenue)
                     .then(nestedRetrieval => retrievedData.concat(nestedRetrieval));
             } else {
                 return retrievedData;
@@ -43,50 +58,12 @@ if (!url.startsWith(expectedPrefix)) {
     console.error("didn't know how to handle URL");
     return Promise.reject("didn't know how to handle URL");
 }
+const isVenue = url.indexOf('venues') > -1;
 const apiUrl = url.slice(0, expectedPrefix.length) + 'api/' + url.slice(expectedPrefix.length);
 
-loadFromIotmUrl(apiUrl).then(retrievedData => {
-    const csvString = Papa.unparse(retrievedData);
+loadFromIotmUrl(apiUrl, isVenue).then(retrievedData => {
+    console.log(retrievedData[0])
+    const columns = isVenue ? venueColumns : bandColumns;
+    const csvString = Papa.unparse(retrievedData, { columns });
     return fs.promises.writeFile("output.csv", csvString);
 });
-
-/*
- * IOTM Users API structure (trimmed to mostly things I'll use):
- * type UsersResponse = {
- *   data: [
- *     {
- *       country: string;
- *       state: string;
- *       city: string;
- *       display_name: string;
- *       bio: string;
- *       localclubs: string;
- *       website: string;
- *       songkick: string;
- *       soundcloud: string;
- *       created_at: string;
- *       genres: {
- *         data: [{
- *             id: number;
- *             genre: string;
- *             order: number;
- *           }];
- *       }
- *     }
- *   ],
- *   meta: {
- *     pagination: {
- *       total: number;
- *       count: number;
- *       per_page: number;
- *       current_page: number;
- *       total_pages: number;
- *       links: {
- *         previous?: string;
- *         next?: string;
- *       }
- *     }
- *   }
- * }
-*/
-
