9ae515fa598decf44e4bc50e1b745978df41372f
Nick Chang <nnchang01@gmail.com>
2022-02-03 14:59:56 -0800

    don't append the /api/ piece multiple times lol

--- iotm/scrape.js
+++ iotm/scrape.js
@@ -3,23 +3,15 @@ const axios = require("axios");
 const fs = require("fs");
 const Papa = require('papaparse');
 
-const expectedPrefix = "https://www.indieonthemove.com/";
-
 /**
  * load the URL
  * re-run with '.meta.pagination.links.next', if it exists
 */
 function loadFromIotmUrl(url) {
-    if (!url.startsWith(expectedPrefix)) {
-        console.error("didn't know how to handle URL");
-        return Promise.reject("didn't know how to handle URL");
-    }
-    const apiUrl = url.slice(0, expectedPrefix.length) + 'api/' + url.slice(expectedPrefix.length);
     return axios.get(apiUrl)
         .then(response => {
             // see data type information at the bottom
             const retrievedData = [];
-            console.log(response.data);
             response.data.data.forEach(bandOrVenue => {
                 retrievedData.push(_.pick(bandOrVenue, ['display_name', 'website', 'songkick', 'soundcloud', 'city', 'state', 'created_at', 'genres', 'bio']));
             })
@@ -37,7 +29,15 @@ if (process.argv.length < 3) {
     return 1;
 }
 
-loadFromIotmUrl(process.argv[2]).then(retrievedData => {
+const url = process.argv[2];
+const expectedPrefix = "https://www.indieonthemove.com/";
+if (!url.startsWith(expectedPrefix)) {
+    console.error("didn't know how to handle URL");
+    return Promise.reject("didn't know how to handle URL");
+}
+const apiUrl = url.slice(0, expectedPrefix.length) + 'api/' + url.slice(expectedPrefix.length);
+
+loadFromIotmUrl(apiUrl).then(retrievedData => {
     const csvString = Papa.unparse(retrievedData);
     return fs.promises.writeFile("output.csv", csvString);
 });
