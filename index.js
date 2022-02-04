b90c217662f389c50dce47105cbe911f889d3ae3
Nick Chang <nnchang01@gmail.com>
2022-02-04 15:06:34 -0800

    FR: only open 10 links at a time

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const openButton = document.querySelector("#open-button");
 
 openButton.addEventListener("click", e => {
     const linksString = urlsBox.value;
-    const linksArray = linksString.split('\n');
-    linksArray
-        .filter(link => link)
+    const linksArray = linksString.split('\n') 
+        .filter(link => link);
+    linksArray.slice(0, 10)
         .forEach(link => open(link, link));
+    urlsBox.value = linksArray.slice(10).join('\n');
 });
--- index.md
+++ index.md
@@ -2,12 +2,13 @@
 getting started with GH pages…
 
 ### Open multiple URLs
+Open many links at once, 10 at a time.
 <textarea
     id="urls-box"
     placeholder="Add links here, one per line."
     rows="10"
     style="font-family: Arial; width: 100%; white-space: pre"
 ></textarea>
-<button id="open-button">Open links</button>
+<button id="open-button">Open 10 links</button>
 
 <script src="index.js"></script>
