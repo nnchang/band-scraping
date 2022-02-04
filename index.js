b3415b403c1f068816c7757649c35f8fd31b30e0
Nick Chang <nnchang01@gmail.com>
2022-02-04 15:02:49 -0800

    self-CR: fix var name

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const urlsBox = document.querySelector("#urls-box");
 const openButton = document.querySelector("#open-button");
 
 openButton.addEventListener("click", e => {
-    const linkString = urlsBox.value;
-    const linksArray = linkString.split('\n');
+    const linksString = urlsBox.value;
+    const linksArray = linksString.split('\n');
     linksArray
         .filter(link => link)
         .forEach(link => open(link, link));
